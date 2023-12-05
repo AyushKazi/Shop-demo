27e0628ffcc3fb8627cd1510706262f30e2cf7af
AyushKazi <ayushkazishrestha@gmail.com>
2023-12-05 14:34:57 +0545

    Added routes and links

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { Nav, Navbar, Container, NavLink } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-
+import { LinkContainer } from "react-router-bootstrap";
 const Header = () => {
   return (
     <>
       <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
           <Container>
-            <Navbar.Brand href="/">Shop Demo</Navbar.Brand>
+            {/* href vako thau ma link container ko {to =""} use garni */}
+            <LinkContainer to="/">
+              <Navbar.Brand>Shop Demo</Navbar.Brand>
+            </LinkContainer>
+
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic=navbar-nav">
               <Nav className="ms-auto">
-                <Nav.Link href="/cart">
-                  <FaShoppingCart /> Cart
-                </Nav.Link>
-                <Nav.Link href="/login">
-                  <FaUser /> Sign In
-                </Nav.Link>
+                <LinkContainer to="/cart">
+                  <Nav.Link>
+                    <FaShoppingCart /> Cart
+                  </Nav.Link>
+                </LinkContainer>
+
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <FaUser /> Sign In
+                  </Nav.Link>
+                </LinkContainer>
               </Nav>
             </Navbar.Collapse>
           </Container>
